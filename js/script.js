557d260ae72f98814f1bac5b9c66ba0af5334e5a
Sighfy <43944097+Sighfy@users.noreply.github.com>
2024-05-01 18:22:58 -0700

    toggle theme
    - added dark and light them toggle, not to actually make the themes

--- js/script.js
+++ js/script.js
@@ -83,3 +83,31 @@ document.addEventListener('DOMContentLoaded', () => {
   });
 
 });
+
+const themeToggle = document.querySelector("#theme-toggle");
+
+themeToggle.addEventListener("click", () => {
+  document.body.classList.contains("light-theme")
+    ? enableDarkMode()
+    : enableLightMode();
+});
+
+function enableDarkMode() {
+  document.body.classList.remove("light-theme");
+  document.body.classList.add("dark-theme");
+}
+
+function enableLightMode() {
+  document.body.classList.remove("dark-theme");
+  document.body.classList.add("light-theme");
+}
+
+function setThemePreference() {
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    enableDarkMode();
+    return;
+  }
+  enableLightMode();
+}
+
+document.onload = setThemePreference();
